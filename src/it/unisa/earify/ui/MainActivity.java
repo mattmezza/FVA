19db36b1d1e77ae9c23c83a3ee1222fdd4b83a97
Matteo Merola <mattmezza@gmail.com>
2014-05-08 17:35:36 +0200

    Aggiunto template per Activity principale

--- src/it/unisa/earify/ui/MainActivity.java
+++ src/it/unisa/earify/ui/MainActivity.java
@@ -1,25 +1,30 @@
 package it.unisa.earify.ui;
 
-import it.unisa.earify.config.Config;
-
 import it.unisa.earify.R;
-import it.unisa.earify.R.id;
-import it.unisa.earify.R.layout;
+import it.unisa.earify.config.Config;
+import it.unisa.earify.extractor.FeatureExtractorAbstraction;
+import it.unisa.earify.extractor.FeaturesExtractor;
+import it.unisa.earify.extractor.IFeatureExtractor;
+
+import java.util.ArrayList;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
+import android.widget.Button;
 import android.widget.TextView;
-import android.os.Build;
 
 public class MainActivity extends Activity {
 
@@ -27,11 +32,31 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		
+		PlaceholderFragment fragment = new PlaceholderFragment();
+		
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+					.add(R.id.container, fragment).commit();
 		}
+		
+		((Button) fragment.getView().findViewById(R.id.select_pictures_button))
+        .setOnClickListener(new OnClickListener() {
+
+            public void onClick(View arg0) {
+
+                // in onCreate or any event where your want the user to
+                // select a file
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,
+                        "Select Picture"), SELECT_PICTURE);
+            }
+        });
+		
+		this.imagesPath = new ArrayList<String>();
 	}
 
 	@Override
@@ -52,6 +77,54 @@ public class MainActivity extends Activity {
 		}
 		return false;
 	}
+	
+	/**
+     * helper to retrieve the path of an image URI
+     */
+    public String getPath(Uri uri) {
+            // just some safety built in 
+            if( uri == null ) {
+                // TODO perform some logging or show user feedback
+                return null;
+            }
+            // try to retrieve the image from the media store first
+            // this will only work for images selected from gallery
+            String[] projection = { MediaStore.Images.Media.DATA };
+            Cursor cursor = managedQuery(uri, projection, null, null, null);
+            if( cursor != null ){
+                int column_index = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                cursor.moveToFirst();
+                return cursor.getString(column_index);
+            }
+            // this is our fallback here
+            return uri.getPath();
+    }
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+            	if (Intent.ACTION_SEND_MULTIPLE.equals(true) && data.hasExtra(Intent.EXTRA_STREAM)) {
+    		    // retrieve a collection of selected images
+    		    ArrayList<Parcelable> list = data.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+    		    
+    		    for (Parcelable parcel : list) {
+    		       Uri uri = (Uri) parcel;
+    		       Uri selectedImageUri = data.getData();
+	               this.imagesPath.add(getPath(selectedImageUri));
+    		   }
+    		}
+            }
+        }
+    }
+	
+	private void extractFeatures() {
+		Config config = new Config(this);
+		
+	}
+	
+	private static final int SELECT_PICTURE = 1;
+	private ArrayList<String> imagesPath;
 
 	/**
 	 * A placeholder fragment containing a simple view.
@@ -66,23 +139,8 @@ public class MainActivity extends Activity {
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_main, container,
 					false);
-			showSettings(rootView);
+			
 			return rootView;
-		}
-		
-		private void showSettings(View view) {
-			Config config = new Config(getActivity());
-			StringBuilder builder = new StringBuilder();
-			builder.append("\n num of img: "
-					+ config.numberOfImagesToUse());
-
-			builder.append("\n use sift: "
-					+ config.sift());
-
-			builder.append("\n use lbp: "
-					+ config.lbp());
-			TextView settingsTextView = (TextView) view.findViewById(R.id.settings_tv);
-			settingsTextView.setText(builder.toString());
 		}
 	}
 
