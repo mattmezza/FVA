e2a7aee72fa481f592bf0317088aff0f3168c229
cbranca <c.branca3@gmail.com>
2014-05-23 15:35:41 +0200

    Aggiunto in Android.mk il mio path commentato

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 
 include /home/simone/Programmi/Android/OpenCV-2.4.9-android-sdk/sdk/native/jni/OpenCV.mk
 #include /Users/Matt/Development/OpenCV-2.4.9-android-sdk/sdk/native/jni/OpenCV.mk
+#include C:\Users\Carlo\Documents\Android\OpenCV-2.4.8-android-sdk\sdk\native\jni\OpenCV.mk
 
 LOCAL_MODULE    := libEarify
 LOCAL_SRC_FILES := lbp.c Earify.c
